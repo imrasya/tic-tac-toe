89399fc76a49968c5b1a92812f06b96aabf29a23
rasyadeans@gmail.com <rasyadeans@gmail.com>
2023-02-05 07:56:13 +0700

    up

--- index.js
+++ index.js
@@ -60,13 +60,13 @@ window.addEventListener('DOMContentLoaded', () => {
     const announce = (type) => {
         switch(type){
             case PLAYERO_WON:
-                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                announcer.innerHTML = 'Player <span class="playerO">O</span> Menang';
                 break;
             case PLAYERX_WON:
-                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                announcer.innerHTML = 'Player <span class="playerX">X</span> Mena';
                 break;
             case TIE:
-                announcer.innerText = 'Tie';
+                announcer.innerText = 'Seri';
         }
         announcer.classList.remove('hide');
     };
